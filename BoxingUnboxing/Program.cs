5aa2409c9b35169e56aa5e42869cb73f9868176c
Sherline Au <au.sherline@gmail.com>
2022-08-01 18:24:43 -0500

    practice

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿List<object> casting = new List<object>();
+
+casting.Add(7);
+casting.Add(28);
+casting.Add(-1);
+casting.Add(true);
+casting.Add("chair");
+int sum = 0;
+
+foreach ( Object item in casting) {
+  Console.WriteLine($"{item}");
+}
+
+foreach (var item in casting) {
+  if (item is int) 
+  {
+    sum += (int)item;
+  }
+}
+Console.WriteLine($"Sum: {sum}");
