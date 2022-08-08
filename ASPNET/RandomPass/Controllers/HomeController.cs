5f9ac5d59fea52d2cfafcc1a38d73f83820c37b8
Sherline Au <au.sherline@gmail.com>
2022-08-08 15:48:03 -0500

    random pass

---
+++ ASPNET/RandomPass/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RandomPass.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        // logic for "counting" number of passcodes generated
+        int? generate = HttpContext.Session.GetInt32("generate");
+        if (generate == null)
+        {
+            HttpContext.Session.SetInt32("generate", 1);
+        } else {
+            generate += 1;
+            HttpContext.Session.SetInt32("generate", (int)generate);
+        }
+
+        // generaing the random passcode
+        string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string passcode = "";
+        Random random = new Random();
+        // change 14 to however long you want the passcode to be
+        for( int i = 0; i < 14; i++)
+        {
+            int idx = random.Next(0,36);
+            passcode += chars[idx];
+        }
+        // temp data used to "temporarily hold our passcode for one redirect"
+        TempData["passcode"] = passcode;
+        TempData["generate"] = HttpContext.Session.GetInt32("generate");
+
+        return View();
+    }
+
+    // clear session / reset attempts
+    [HttpGet("/clear")]
+    public IActionResult Clear()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+
+    
+}
